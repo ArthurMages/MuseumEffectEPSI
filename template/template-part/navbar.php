7dd42429a3cc5761a58ecbe205bf2de1cadfc93c
Ronanalp <alphandronan.pro@gmail.com>
2024-12-17 13:52:51 +0100

    push :(

--- template/template-part/navbar.php
+++ template/template-part/navbar.php
@@ -3,18 +3,17 @@
         <ul class="menu">
             <li><a id="one-of-type" href="index.php?page=home">Museum Effect</a></li>
             <li><a id="two-of-type" href="index.php?page=home">Accueil</a></li>
-            <li><a id="two-of-type" href="index.php?page=contact">Contact</a></li>
             <li><a id="three-of-type" href="#">Galerie</a></li>
         </ul>
    
         <!-- Phone -->
-         <a href="index.php?page=home" class="burger-title">Museum Effect</a>
+         <a href="index.php?page=home" class="burger-title">Covering3d</a>
         <input type="checkbox" id="burger-toggle" class="burger-toggle">
         <label for="burger-toggle" class="burger-icon">☰</label>
 
         <ul class="burger-menu">
             <li><a href="index.php?page=home">Accueil</a></li>
-            <li><a href="index.php?page=contact">Contact</a></li>
+            <li><a href="#">Contacter</a></li>
         </ul>
     </nav>
 </header>
